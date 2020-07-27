87f18a0ebcdd184159de942330444cb4cb0b8364
Amit Kumar Samanta <Amit Kumar Samanta@ip-192-168-107-1.ec2.internal>
2020-07-27 15:18:21 +0530

    My First Commit

--- wc-slim-api/src/main/java/com/mediaplatform/main/WcSlimApiApplication.java
+++ wc-slim-api/src/main/java/com/mediaplatform/main/WcSlimApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WcSlimApiApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(WcSlimApiApplication.class, args);
 	}
